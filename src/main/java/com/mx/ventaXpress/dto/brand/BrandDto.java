7953a927d816209db3383030ccb4ef4076b383e2
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2024-08-29 20:40:26 -0600

    [Update] creating new functions

---
+++ src/main/java/com/mx/ventaXpress/dto/brand/BrandDto.java
@@ -0,0 +1,25 @@
+package com.mx.ventaXpress.dto.brand;
+
+import com.mx.ventaXpress.model.brand.Brand;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BrandDto {
+    private long id;
+    private String nombre;
+    private Boolean status;
+
+    public Brand getBrand(){
+        return new Brand(
+                getId(),
+                getNombre(),
+                getStatus()
+        );
+    }
+}
